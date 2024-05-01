0b0fec6e12643a5759f0b881f1d2a418e005a1f8
Smruthul12 <msmruthul2002@gmail.com>
2024-05-01 23:05:07 +0530

    commit

---
+++ build/src/scenes/scripts-map.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scriptsMap = void 0;
+/**
+ * Defines the map of all available scripts in the project.
+ */
+exports.scriptsMap = {
+    "src/scenes/scene/player.ts": require("./scene/player"),
+    "src/scenes/scene/playerCamera.ts": require("./scene/playerCamera"),
+};
+//# sourceMappingURL=scripts-map.js.map
